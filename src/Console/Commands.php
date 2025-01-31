2433a63d39af34214ecd019f2a0f38f4f76fe109
Daniel <easelpainting@gmail.com>
2025-01-31 16:28:06 +0100

    refactor console command structure and add output messages for resource publishing

--- src/Console/Commands.php
+++ src/Console/Commands.php
@@ -28,19 +28,40 @@ class Commands
                 $sourceDir = __DIR__ . '/../../resources';
                 $targetDir = getcwd() . '/resources';
 
+                $output->writeln("Source Directory: $sourceDir");
+                $output->writeln("Target Directory: $targetDir");
+
+                if (!$filesystem->exists($sourceDir)) {
+                    $output->writeln("Source directory does not exist: $sourceDir");
+                    return Command::FAILURE;
+                }
+
                 if (!$filesystem->exists($targetDir)) {
                     $filesystem->mkdir($targetDir);
+                    $output->writeln("Created target directory: $targetDir");
                 }
 
                 $filesystem->mirror($sourceDir . '/templates', $targetDir . '/templates', null, ['override' => false]);
+                $output->writeln("Copied templates from $sourceDir/templates to $targetDir/templates");
+
                 $filesystem->mirror($sourceDir . '/modules', $targetDir . '/modules', null, ['override' => false]);
+                $output->writeln("Copied modules from $sourceDir/modules to $targetDir/modules");
 
                 // Copy index.php to the project root
                 $indexFileSource = __DIR__ . '/../../index.php';
                 $indexFileTarget = getcwd() . '/index.php';
 
+                $output->writeln("Index File Source: $indexFileSource");
+                $output->writeln("Index File Target: $indexFileTarget");
+
+                if (!$filesystem->exists($indexFileSource)) {
+                    $output->writeln("Index file does not exist: $indexFileSource");
+                    return Command::FAILURE;
+                }
+
                 if (!$filesystem->exists($indexFileTarget)) {
                     $filesystem->copy($indexFileSource, $indexFileTarget);
+                    $output->writeln("Copied index.php to $indexFileTarget");
                 }
 
                 $output->writeln('Resources published successfully.');
