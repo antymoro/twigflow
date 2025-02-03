30427fc140a1e47fbcd6aa0da2bfe07b00672c64
Daniel <easelpainting@gmail.com>
2025-02-03 17:38:05 +0100

    refactor: add slugify method to SanityCmsClient for improved module naming

--- src/CmsClients/SanityCmsClient.php
+++ src/CmsClients/SanityCmsClient.php
@@ -63,7 +63,35 @@ class SanityCmsClient implements CmsClientInterface
             }
         }
 
-        $page['modules'] = $modulesArray;
+        $page['modules'] = $this->slugify($module['_type']);;
         return $page;
     }
+
+
+    private function slugify(string $text): string
+{
+    // Replace non-letter or digits by -
+    $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+
+    // Transliterate
+    $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+    // Remove unwanted characters
+    $text = preg_replace('~[^-\w]+~', '', $text);
+
+    // Trim
+    $text = trim($text, '-');
+
+    // Remove duplicate -
+    $text = preg_replace('~-+~', '-', $text);
+
+    // Lowercase
+    $text = strtolower($text);
+
+    if (empty($text)) {
+        return 'n-a';
+    }
+
+    return $text;
+}
 }
