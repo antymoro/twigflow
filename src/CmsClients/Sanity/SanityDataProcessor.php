0f4b04b0b7a360ca7234508c7727c2040bdfbc77
Daniel Wozniak <easelpainting@gmail.com>
2026-02-09 16:00:23 +0100

    Add 'linkButton' to BLOCK_TYPE_MAP in SanityDataProcessor

--- src/CmsClients/Sanity/SanityDataProcessor.php
+++ src/CmsClients/Sanity/SanityDataProcessor.php
@@ -14,6 +14,7 @@ class SanityDataProcessor
         'video' => 'video',
         'quoteBlock' => 'quote',
         'accordion' => 'accordion',
+        'linkButton' => 'linkButton',
     ];
 
     private RequestContext $context;
